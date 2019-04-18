0c62df146930cc277b7a6b0d51bccf0f164c2820
高木　慎太郎 <takagi.sh1ntar0@gmail.com>
2019-04-18 18:13:25 +0900

    Update ip.py

--- cgi-bin/ip.py
+++ cgi-bin/ip.py
@@ -1,3 +1,20 @@
-#!/root/.pyenv/shims/python3
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+import socket;
+from datetime import datetime
+
+ip = [(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
+
+
+print ("Content-type: text/html")
+print
+print ("<html>")
+print ("<font size=\"7\">")
+print (datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+print ("</br>")
+print ("</br>")
+print (ip)
+print ("</font>")
+print ("</html>")
 
